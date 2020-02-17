80b43304d757811092fa267d98c4d2bcd640fb7f
Canco <francobaldassarerodriguez@gmail.com>
2020-02-17 14:03:35 -0300

    Cambio
    Menú, aún no se puede

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,8 +1,16 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
+$(document).ready(function() {
+  $('#existing-content-menu').sidr({
+    name: 'sidr-existing-content',
+    source: '#demoheader, #demo-content'
+  });
+  $('#remote-content-menu').sidr({
+    name: 'sidr-remote-content',
+    source: 'http://www.example.com/remote-menu.html'
+  });
+  $('#callback-menu').sidr({
+    name: 'sidr-callback',
+    source: function(name) {
+      return '<h1>' + name + ' menu</h1><p>Yes! You can use a callback too ;)</p>';
     }
-  }
+  });
+});
